aac8a04a9dc3d5db77567889173fae3055d5f83e
Tim Koh <tim.koh@icloud.com>
2026-02-22 17:33:13 +0000

    Added displaying player data each turn i.e. correct letters, guessed letters + added winner messaging to console

--- Hangman.py
+++ Hangman.py
@@ -2,12 +2,12 @@
 Tim Koh's Sigma Capstone Project: Hangman in Python
 
 Basic structure
-1. Com chooses a random word from a collection of words?
-2. Player guesses letters
-3. Player has limited guesses
-    - correct + incorrect chosen letters are shown
-    - hangman graphic is displayed (growing with incorrect guesses)
-4. Player wins / loses and is offered to play again or quit program
+    1. Com chooses a random word from a collection of words?
+    2. Player guesses letters
+    3. Player has limited guesses
+        - correct + incorrect chosen letters are shown
+        - hangman graphic is displayed (growing with incorrect guesses)
+    4. Player wins / loses and is offered to play again or quit program
 
 Program must be run in the terminal.
 
@@ -15,6 +15,13 @@ Credit to MichaelWehar for the txt file of 5000 common English words (license: p
 --> https://github.com/MichaelWehar/Public-Domain-Word-Lists.git
 
 Words of length 2 - 14.
+
+Things to add:
+    1. Display hangman
+    2. Offer to play again
+
+    3. Collect overall game data (games won, lost etc)
+    4. Offer difficulty levels (more lives etc)
 
 """
 
@@ -29,6 +36,10 @@ def get_word_list(filepath: pathlib.Path) -> list[str]:
     # function to read word_list from file using pathlib
     with filepath.open("r") as file:
         return file.read().splitlines()
+
+# # open 5000_words.txt file and save words into list
+# with open("5000_words.txt", "r") as file:
+#     WORD_LIST = file.read().splitlines()
 
 FILE_NAME = "5000_words.txt"
 WORD_LIST = get_word_list(Path.cwd() / FILE_NAME)
@@ -54,9 +65,32 @@ def get_word_length() -> int:
         except ValueError:
             print("That's not a positive integer. Try again.")
 
-# # open 5000_words.txt file and save words into list
-# with open("5000_words.txt", "r") as file:
-#     WORD_LIST = file.read().splitlines()
+def get_guess(previous_guesses: list[str]) -> str:
+    # function to get guess of letter from player
+    # with error handling for non-alphabetic chars, multiple chars and previously guessed letters
+    while True:
+        guess = str(input("What's your guess? "))
+        if not guess.isalpha():
+            print("Guess a letter. Try again.")
+        elif len(guess) > 1:
+            print("Just one letter please. Try again.")
+        elif guess in previous_guesses:
+            print("Already guessed that one. Try another one.")
+        else:
+            return guess.lower()
+        
+def get_letter_positions(ANSWER: str, guess: str) -> list[int]:
+    # function to find position of correctly guessed letters in answer str
+    # returns the positions as list of int indices
+    letter_positions = []
+    start_of_search = 0
+    
+    while True:
+        start_of_search = ANSWER.find(guess,start_of_search)
+        if start_of_search == -1:
+            return letter_positions
+        letter_positions.append(start_of_search)
+        start_of_search += 1
 
 print("""
     
@@ -77,46 +111,59 @@ print("How long a word would you like to guess?")
 word_length = get_word_length()
 ANSWER = find_word(WORD_LIST, word_length)
 
-guessed_letters = "_ "*word_length
-guessed_letters = guessed_letters[:-1]  # remove the last space
+# save the guessed letters as a list of chars
+# strings are immutable and we want to update for the player when they guess correctly
+correct_letters = ["_"]*word_length
 
-print(f"This is your word: {guessed_letters}. Let's start!")
+print("Let's start!")
 
 # begin a loop - ending when the word is guessed correctly or the hangman is finished
-lives = 8
+MAX_LIVES = 8
 lives_lost = 0
 previous_guesses = []
 
-def get_guess(previous_guesses: list[str]) -> str:
-    while True:
-        guess = str(input("What's your guess? "))
-        if not guess.isalpha():
-            print("Guess a letter. Try again.")
-        elif len(guess) > 1:
-            print("Just one letter please. Try again.")
-        elif guess in previous_guesses:
-            print("Already guessed that one. Try another one.")
-        else:
-            return guess.lower()
-
 while True:
-    print(f"\nYou have {lives - lives_lost} lives left.")
+    # remind player of lives and previous guesses
+    print("\n===== Player data =====")
+    print(f"\nYou have {MAX_LIVES - lives_lost} lives left.")
 
+    previous_guesses_str = " ".join(previous_guesses)
+    print(f"So far you have guessed: {previous_guesses_str}\n")
+
+    # get a guess from the player
     guess = get_guess(previous_guesses)
+
+    # update previously guessed letters for player
     previous_guesses.append(guess)
 
+    # check if the player is right or wrong
     if guess in ANSWER:
-        print("That's right!")
+        print("\nThat's right.")
+        # check where the guessed letter is in the answer
+        letter_positions = get_letter_positions(ANSWER, guess)
 
-
-        
+        # update known letters for player
+        for index in letter_positions:
+            correct_letters[index] = guess
     else:
         print("Not that one.")
         lives_lost += 1
-    
-    if lives_lost == 8:
-        print("===== Game over =====")
+
+    # check if the player has won
+    if "_" not in correct_letters:
+        print("\n===== Winner =====")
+        print(f"\nThe word was: *** {ANSWER} ***.")
         break
+    # check if the player has lost
+    elif lives_lost == MAX_LIVES:
+        print("\n===== Game over =====")
+        print("You ran out of lives.")
+        break
+    # else give the player an update on the game for the next turn
+    else:
+        correct_letters_str = " ".join(correct_letters)
+        print(f"\nThis is what you've got so far: \n\n{correct_letters_str}")
+
 
 """
 Question to consider - how do we build and display a hangman?
