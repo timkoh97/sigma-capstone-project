c4d844159044740b0d16024fc2188dc4d1742e04
Tim Koh <tim.koh@icloud.com>
2026-02-22 18:59:51 +0000

    Added features for printing the incrementing hangman and choosing a difficulty (easy/medium/hard)

--- Hangman.py
+++ Hangman.py
@@ -17,7 +17,7 @@ Credit to MichaelWehar for the txt file of 5000 common English words (license: p
 Words of length 2 - 14.
 
 Things to add:
-    1. Display hangman
+    1. Display hangman - done
     2. Offer to play again
 
     3. Collect overall game data (games won, lost etc)
@@ -30,7 +30,6 @@ from pathlib import Path
 import random
 # 1. constants - use caps
 # 2. pathlib is your friend
-
 
 def get_word_list(filepath: pathlib.Path) -> list[str]:
     # function to read word_list from file using pathlib
@@ -52,9 +51,11 @@ def find_word(word_list: list[str], word_length: int = 8) -> str:
             return word
 
 def get_word_length() -> int:
+    print("Choose your word length.")
     # function to retrieve length of answer from player with error handling
     while True:
-        word_length = input("\nPlease enter a whole number between 5 and 14: ")
+        word_length = input("\nEnter a whole number between 5 and 14: ")
+        print("")
         try:
             word_length = int(word_length)
             if word_length >= 5 and word_length <= 14:
@@ -71,11 +72,11 @@ def get_guess(previous_guesses: list[str]) -> str:
     while True:
         guess = str(input("What's your guess? "))
         if not guess.isalpha():
-            print("Guess a letter. Try again.")
+            print("Guess a letter. Try again.\n")
         elif len(guess) > 1:
-            print("Just one letter please. Try again.")
+            print("Just one letter please. Try again.\n")
         elif guess in previous_guesses:
-            print("Already guessed that one. Try another one.")
+            print("Already guessed that one. Try another one.\n")
         else:
             return guess.lower()
         
@@ -92,79 +93,196 @@ def get_letter_positions(ANSWER: str, guess: str) -> list[int]:
         letter_positions.append(start_of_search)
         start_of_search += 1
 
-print("""
-    
-================== Welcome to ==================
- _   _    _    _   _  ____ __  __    _    _   _ 
-| | | |  / \  | \ | |/ ___|  \/  |  / \  | \ | |
-| |_| | / _ \ |  \| | |  _| |\/| | / _ \ |  \| |
-|  _  |/ ___ \| |\  | |_| | |  | |/ ___ \| |\  |
-|_| |_/_/   \_\_| \_|\____|_|  |_/_/   \_\_| \_|
+def print_player_data(max_lives: int, lives_lost: int, previous_guesses: list[str], correct_letters: list[str], difficulty_offset: int) -> None:
+    # prints all player data - lives left, construction of gallows, guessed letters and correct letters so far
+    print("\n===== Player data =====\n")
 
-================================ by Tim Koh ===
-      """)
+    if max_lives == 10:
+        print("Difficulty: easy")
+    elif max_lives == 8:
+        print("Difficulty: medium")
+    elif max_lives == 6:
+        print("Difficulty: hard")
 
-
-# ask for difficulty (answer length) and check validity - integer between 5 and 14
-print("How long a word would you like to guess?")
-
-word_length = get_word_length()
-ANSWER = find_word(WORD_LIST, word_length)
-
-# save the guessed letters as a list of chars
-# strings are immutable and we want to update for the player when they guess correctly
-correct_letters = ["_"]*word_length
-
-print("Let's start!")
-
-# begin a loop - ending when the word is guessed correctly or the hangman is finished
-MAX_LIVES = 8
-lives_lost = 0
-previous_guesses = []
-
-while True:
-    # remind player of lives and previous guesses
-    print("\n===== Player data =====")
-    print(f"\nYou have {MAX_LIVES - lives_lost} lives left.")
+    lives_left = max_lives - lives_lost
+    if lives_left != 1:
+        print(f"\nYou have {lives_left} lives left.")
+    else:
+        print(f"\nYou have {lives_left} life left.")
 
     previous_guesses_str = " ".join(previous_guesses)
-    print(f"So far you have guessed: {previous_guesses_str}\n")
+    print(f"So far you have guessed: {previous_guesses_str}")
+    print(HANGMAN_DIAGRAMS[lives_lost + difficulty_offset])
 
-    # get a guess from the player
-    guess = get_guess(previous_guesses)
+    correct_letters_str = " ".join(correct_letters)
+    print(f"\n\n{correct_letters_str}")
+    
+    print("\n=======================\n")
 
-    # update previously guessed letters for player
-    previous_guesses.append(guess)
+def get_difficulty() -> int:
+    # function to ask for difficulty level - easy (10 lives), medium (8 lives) or hard (6 lives)
+    # return max lives
+    
+    print("Choose your difficulty: easy (10 lives) / medium (8 lives) / hard (6 lives)\n")
+    
+    while True:
+        options = ["e","m","h"]
+        guess = str(input("Easy(e), medium(m) or hard(h): "))
+        print("")
+        if guess not in options:
+            print("Invalid. Choose again.\n")
+        elif guess == "e":
+            print("Understood.")
+            return 10
+        elif guess == "m":
+            print("Understood.")
+            return 8
+        elif guess == "h":
+            print("Understood.")
+            return 6
+        
+HANGMAN_DIAGRAMS = ["""
+        
+    ***BEWARE***
+       gallows
+        under
+    construction     
 
-    # check if the player is right or wrong
-    if guess in ANSWER:
-        print("\nThat's right.")
-        # check where the guessed letter is in the answer
-        letter_positions = get_letter_positions(ANSWER, guess)
-
-        # update known letters for player
-        for index in letter_positions:
-            correct_letters[index] = guess
-    else:
-        print("Not that one.")
-        lives_lost += 1
-
-    # check if the player has won
-    if "_" not in correct_letters:
-        print("\n===== Winner =====")
-        print(f"\nThe word was: *** {ANSWER} ***.")
-        break
-    # check if the player has lost
-    elif lives_lost == MAX_LIVES:
-        print("\n===== Game over =====")
-        print("You ran out of lives.")
-        break
-    # else give the player an update on the game for the next turn
-    else:
-        correct_letters_str = " ".join(correct_letters)
-        print(f"\nThis is what you've got so far: \n\n{correct_letters_str}")
+             ""","""
 
 
-"""
-Question to consider - how do we build and display a hangman?
-"""
+        
+        
+        
+        
++===========+""","""
+
+        ||
+        ||
+        ||
+        ||
+        ||
++===========+""","""             
+  ==+===++
+        ||
+        ||
+        ||
+        ||
+        ||
++===========+""", """
+  ==+===++
+    |   ||
+        ||
+        ||
+        ||
+        ||
++===========+""", """
+  ==+===++
+    |   ||
+    O   ||
+        ||
+        ||
+        ||
++===========+""", """
+  ==+===++
+    |   ||
+    O   ||
+    |   ||
+        ||
+        ||
++===========+""", """
+  ==+===++
+    |   ||
+    O   ||
+   /|   ||
+        ||
+        ||
++===========+""", """
+  ==+===++
+    |   ||
+    O   ||
+   /|\  ||
+        ||
+        ||
++===========+""", """
+  ==+===++
+    |   ||
+    O   ||
+   /|\  ||
+   /    ||
+        ||
++===========+""", """
+  ==+===++
+    |   ||
+    O   ||
+   /|\  ||
+   / \  ||
+        ||
++===========+"""]
+
+def game() -> None:
+    print("""
+        
+    ================== Welcome to ==================
+    _   _    _    _   _  ____ __  __    _    _   _ 
+    | | | |  / \  | \ | |/ ___|  \/  |  / \  | \ | |
+    | |_| | / _ \ |  \| | |  _| |\/| | / _ \ |  \| |
+    |  _  |/ ___ \| |\  | |_| | |  | |/ ___ \| |\  |
+    |_| |_/_/   \_\_| \_|\____|_|  |_/_/   \_\_| \_|
+
+    ================================ by Tim Koh ===
+        """)
+
+    # ask for answer length and check validity - integer between 5 and 14
+    word_length = get_word_length()
+    ANSWER = find_word(WORD_LIST, word_length)
+
+    # save the guessed letters as a list of chars
+    # strings are immutable and we want to update for the player when they guess correctly
+    correct_letters = ["_"]*word_length
+
+    # ask for difficulty level - easy (10 lives), medium (8 lives) or hard (6 lives)
+    max_lives = get_difficulty()
+    difficulty_offset = 10 - max_lives
+
+    print("Begin.")
+
+    # begin a loop - ending when the word is guessed correctly or the hangman is finished
+    lives_lost = 0
+    previous_guesses = []
+
+    while True:
+        print_player_data(max_lives, lives_lost, previous_guesses, correct_letters, difficulty_offset)
+
+        # get a guess from the player
+        guess = get_guess(previous_guesses)
+
+        # update previously guessed letters for player
+        previous_guesses.append(guess)
+
+        # check if the player is right or wrong
+        if guess in ANSWER:
+            print("\n*** That's right. ***")
+            # check where the guessed letter is in the answer
+            letter_positions = get_letter_positions(ANSWER, guess)
+
+            # update known letters for player
+            for index in letter_positions:
+                correct_letters[index] = guess
+        else:
+            print("\n::: Not that one. :::")
+            lives_lost += 1
+
+        # check if the player has won
+        if "_" not in correct_letters:
+            print("\n===== Winner =====")
+            print(f"\nThe word was: *** {ANSWER} ***.")
+            break
+        # check if the player has lost
+        elif lives_lost == max_lives:
+            print("\n===== Game over =====\n")
+            print(HANGMAN_DIAGRAMS[lives_lost + difficulty_offset])
+            print("\nYou ran out of lives.\n")
+            break
+
+game()
